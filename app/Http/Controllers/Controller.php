3ad772615662965b407e42fe883c9a5b38478d70
Yujin Jeong <te0209494@gmail.com>
2025-03-27 11:50:51 -0700

    Fixed url

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,8 @@ use OpenApi\Attributes as OA;
 #[
     OA\Info(version: "1.0.0", description: "School API Documentation", title: "School API Documentation"),
     OA\Server(url: 'http://127.0.0.1:8000/api', description: "local server"), 
-    OA\Server (url: 'http://staging.example.com', description: "staging server"), 
-    OA\Server(url: 'http://example.com', description: "production server"),
+    OA\Server (url: 'https://laravel-swagger-yj.azurewebsites.net/', description: "staging server"), 
+    OA\Server(url: 'https://laravel-swagger-yj.azurewebsites.net/', description: "production server"),
     OA\SecurityScheme (securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header"),
 ]
 
